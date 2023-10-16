9453736f8a16b5af32926feef2049117bcf98ad4
Mekerjunior <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>
2023-10-16 13:44:54 -0300

    Aula5

---
+++ Aula5/main.py
@@ -0,0 +1,12 @@
+
+
+temperauta = 31
+
+if temperauta < 0 :
+    print("Esta congelando la fora")
+elif temperauta >= 0 and temperauta <= 25 : 
+    print("Esta frio la fora")
+elif temperauta > 25  and temperauta < 30 :
+    print("Esta agradavel")
+else:
+    print("Esta quente la fora")    
