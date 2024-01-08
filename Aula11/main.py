777eb45930f729e4b4d95a3f643a722bc13d6c69
Mekerjunior <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>
2024-01-08 19:27:51 -0300

    Aula 11 Funções

---
+++ Aula11/main.py
@@ -0,0 +1,12 @@
+list=["Rapha","Vini","Tiago","Marcelo"]
+
+num=[1,4,5,7]
+
+def listados(num, list=["Vazio"]):
+   print(num*10)
+   for nome in list:
+        print(nome)
+
+#listados(list)  
+#listados(num)  
+listados(5)
