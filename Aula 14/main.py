77c1995c75a9cfc809d8d8f9012099795f7e792b
Mekerjunior <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>
2024-05-05 19:40:17 -0300

    Live POO

---
+++ Aula 14/main.py
@@ -0,0 +1,21 @@
+
+# print(resultado = 10 / 0)
+
+# try:
+#    resultado = 10 / 0
+#    print(resultado)
+# except ZeroDivisionError:
+#    print("Nao e possivel dividir por zero.")
+
+# print("Tudo certo")
+
+# lista =[1,2,3]
+# print(lista[4])
+
+try:
+ lista =[10,20,30]
+ print(lista[2])
+except IndexError:
+   print("Indice inexistente")
+
+print("Tudo certo")
